58372364f83f1f8f7a1fc6edac1fd259f33c2dc1
basiji <40441882+basiji@users.noreply.github.com>
2018-09-13 11:58:38 +0430

    x

--- modules/router_v1.js
+++ modules/router_v1.js
@@ -9,12 +9,12 @@ module.exports = function(connection){
     var router = express.Router();
 
     // Register / Login
-    router.get('/register', function(req, res){
+    router.post('/register', function(req, res){
         register(req, res, connection);
     });
 
     // Wallet
-    router.get('/wallet', function(req, res){
+    router.post('/wallet', function(req, res){
         wallet(req, res, connection);
     });
 
